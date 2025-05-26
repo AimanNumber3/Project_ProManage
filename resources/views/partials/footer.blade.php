8f4f05dc09b80c58755caeb5db41cca93738d087
AimanNumber3 <147214166+AimanNumber3@users.noreply.github.com>
2025-05-26 11:07:26 +0800

    push

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-light text-center py-3 mt-auto">
-    <div class="container">
-        <span class="text-muted">© {{ date('Y') }} Sistem Manajemen Tugas - Laravel & Bootstrap</span>
-    </div>
-</footer>
